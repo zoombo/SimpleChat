307dde9785a89b9dd1320dfed4e33afd8c49a9e3
zoombo <AntiduPb@yandex.ru>
2016-12-24 23:10:05 +0300

    pass

--- SimpleChat.py
+++ SimpleChat.py
@@ -1,30 +1,32 @@
 import tkinter
-import tkinter.ttk as tkink
+#import tkinter.ttk
+import pygubu
 
-def exitMw():
-    exit()
 
-def hideShow():
-    if firstLabel.winfo_viewable():
-        firstLabel.grid_remove()
-    else:
-        firstLabel.grid()
+mainFrame = tkinter.Tk()
+mainFrame.resizable(False, False)
+# Подключаем Рисовалку гуев
+builder = pygubu.Builder() 
+# Загружаем в Рисовалку файл с описание что и где рисовать 
+builder.add_from_file('C:/Users/Dimasik/Projects/Python3_5_2/SimpleChat/MainGUI.ui')
+# Загружаем объект mainFrame гуев в наше главное окно mainWindow  
+mainFrame = builder.get_object('mainFrame', mainFrame)
+#builder.connect_callbacks(mainFrame)
 
-# Init main window
-mainWindow = tkinter.Tk()
-# Set minimum windows size  
-mainWindow.minsize(600, 400)
+# Добываем объекты типа Text и прикрепляем соответствующие Scrollbar'ы
+    # Тут окно вывода изображения
+messageViewer = builder.get_object('messageViewer', mainFrame)
+messageViewer_scroll = builder.get_object('messageViewer_scroll', mainFrame)
+        # Прикрепляем скролл
+messageViewer_scroll['command'] = messageViewer.yview
+messageViewer['yscrollcommand'] = messageViewer_scroll.set
+    # Тут окно подключенных пользователей
+membersList = builder.get_object('membersList', mainFrame)
+membersList_scroll = builder.get_object('membersList_scroll', mainFrame)
+        # Прикрепляем скролл
+membersList_scroll['command'] = membersList.yview
+membersList['yscrollcommand'] = membersList_scroll.set
 
-okButton = tkinter.ttk.Button(mainWindow, text = "Exit", command = exitMw)
-okButton.grid()
-okButton.place(x = 510, y = 360)
-okButton.focus()
+mainFrame.mainloop()
 
-hideButton = tkink.Button(mainWindow, text = "Hide\Show", command = hideShow)
-hideButton.grid()
-hideButton.place(x = 430, y = 360)
 
-firstLabel = tkink.Label(text = "I am First Label :)")
-firstLabel.grid()
-
-mainWindow.mainloop()
